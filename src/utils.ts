1c8498d19ce7cc6568744290f1089cbd9412d05d
Eunovo <eunovo9@gmail.com>
2022-03-06 17:43:10 +0100

    Add convert from btc to satoshis

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function convertSatoshisToBTC(amountInSatoshis: number) {
     return amountInSatoshis / 100000000;
 }
+
+export function convertBTCToSatoshis(amountInBTC: number) {
+    return amountInBTC * 100000000;
+}
