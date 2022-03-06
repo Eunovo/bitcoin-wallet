506a7cab8271ac687c94bda609a4b5afa6d0e832
Eunovo <eunovo9@gmail.com>
2022-03-06 16:27:19 +0100

    Adjust font for small screens

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Container, Backdrop, Box, Button, Slide, Typography } from "@mui/material";
+import { Container, Backdrop, Box, Button, Slide, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { Send as SendIcon, CallReceived as ReceiveIcon } from "@mui/icons-material";
 import BitcoinLogo from "../assets/bitcoin-btc-logo.svg";
 import { CopyableContent } from "../components/CopyUtils";
@@ -12,6 +12,8 @@ import { useObservable } from "../Observable";
 import { convertSatoshisToBTC } from "../utils";
 
 export const Main: React.FC = () => {
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));    
     const { wallet } = useGlobalState();
     const [state, setState] = useState({ send: false, receive: false });
     const toggle = (field: string) => setState((s: any) => ({ ...s, [field]: !s[field] }));
@@ -19,11 +21,11 @@ export const Main: React.FC = () => {
     const transactions: Transaction[] = [];
 
     return <Box py={10}>
-        <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', px: 2 }}>
 
             <Box>
                 <Typography color='textSecondary' variant='body2' sx={{ px: 1 }}>Tap to Copy</Typography>
-                <CopyableContent sx={{ fontSize: '24px', px: 1, py: 0.5 }}>{wallet?.getReceiveAddr() || ""}</CopyableContent>
+                <CopyableContent sx={{ fontSize: 'clamp(18px, 4vw, 24px)', px: 1, py: 0.5 }}>{wallet?.getReceiveAddr() || ""}</CopyableContent>
             </Box>
 
             <Box display='flex' alignItems='center' justifyContent='center' mt={4}>
@@ -31,7 +33,7 @@ export const Main: React.FC = () => {
                     objectFit: 'cover',
                     width: '70px', height: '70px'
                 }} />
-                <Typography sx={{ ml: 2, fontSize: '68px' }}>{wallet && <Balance wallet={wallet} />}</Typography>
+                <Typography sx={{ ml: 2, fontSize: 'clamp(48px, 10vw, 68px)' }}>{wallet && <Balance wallet={wallet} />}</Typography>
             </Box>
 
             <Box display='flex' alignItems='center' justifyContent='center' mt={2} mb={8}>
@@ -50,7 +52,7 @@ export const Main: React.FC = () => {
 
             </Box>
 
-            <Typography variant='h3' sx={{ position: 'relative' }}>
+            <Typography variant={isSmallScreen ? 'h5' : 'h3'} sx={{ position: 'relative' }}>
                 Transaction History
 
                 <Box
@@ -69,9 +71,9 @@ export const Main: React.FC = () => {
         <Backdrop open={state.send} onClick={() => toggle('send')} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
             <Slide in={state.send} direction='up'>
                 <Box bgcolor='white' borderRadius={'5% 5% 0% 0%'} onClick={(e) => { e.stopPropagation(); }}
-                    position='absolute' pt={10} top={'30%'} left={0} right={0} bottom={0}>
+                    position='absolute' pt={10} px={2} top={'30%'} left={0} right={0} bottom={0}>
                     <Box mx='auto' width='100%' maxWidth={'40rem'}>
-                        <Typography variant="h3">Send Coins</Typography>
+                        <Typography variant={isSmallScreen ? 'h5' : 'h3'}>Send Coins</Typography>
 
                         <Box mt={4}>
                             <SendCoins wallet={wallet!} handleBack={() => toggle('send')} />
